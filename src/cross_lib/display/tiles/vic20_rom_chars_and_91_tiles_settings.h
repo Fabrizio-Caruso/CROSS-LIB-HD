a9c2453bac90d874d07162a9b992b76e6a84f660
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2026-03-16 18:27:58 +0100

    supervision + experiments with vic20 and c16

---
+++ src/cross_lib/display/tiles/vic20_rom_chars_and_91_tiles_settings.h
@@ -0,0 +1,116 @@
+
+// BLUE
+#define _TILE_0 0x79
+#define _TILE_1 0x6C
+#define _TILE_2 0x7B
+#define _TILE_3 0x7A
+// #define _PLAYER _PLAYER_DOWN
+
+#define _TILE_9 0x6D
+
+// YELLOW
+#define _TILE_7 0x77
+#define _TILE_12 0x74
+#define _TILE_13 0x75
+
+// GREEN
+#define _TILE_8  0x6F
+
+// RED
+#define _TILE_5 0x78
+
+// CYAN
+#define _TILE_10 0x6E
+#define _TILE_11 0x73
+
+
+// WHITE
+#define _TILE_4 0x76
+
+#define _TILE_6 0x7F
+
+#define _TILE_14 0x71
+
+#define _TILE_15 0x7C
+
+#define _TILE_16 0x70
+
+#define _TILE_17 0x7D
+
+#define _TILE_18 0x7E
+
+#define _TILE_19 0x64
+#define _TILE_20 0x65
+#define _TILE_21 0x66
+#define _TILE_22 0x67
+#define _TILE_23 0x68
+#define _TILE_24 0x69
+#define _TILE_25 0x6A
+#define _TILE_26 0x6B
+
+#define _TILE_27 96
+#define _TILE_28 97
+#define _TILE_29 98
+#define _TILE_30 99
+#define _TILE_31 100
+#define _TILE_32 101
+#define _TILE_33 102
+#define _TILE_34 103
+#define _TILE_35 104
+#define _TILE_36 105
+#define _TILE_37 106
+#define _TILE_38 107
+#define _TILE_39 108
+#define _TILE_40 109
+#define _TILE_41 110
+#define _TILE_42 111
+#define _TILE_43 112
+#define _TILE_44 113
+#define _TILE_45 114
+#define _TILE_46 115
+#define _TILE_47 116
+#define _TILE_48 117
+#define _TILE_49 118
+#define _TILE_50 119
+#define _TILE_51 120
+#define _TILE_52 121
+#define _TILE_53 122
+#define _TILE_54 123
+#define _TILE_55 124
+#define _TILE_56 125
+#define _TILE_57 126
+#define _TILE_58 127
+#define _TILE_59 128
+#define _TILE_60 129
+#define _TILE_61 130
+#define _TILE_62 131
+#define _TILE_63 132
+#define _TILE_64 133
+#define _TILE_65 134
+#define _TILE_66 135
+#define _TILE_67 136
+#define _TILE_68 137
+#define _TILE_69 138
+#define _TILE_70 139
+#define _TILE_71 140
+#define _TILE_72 141
+#define _TILE_73 142
+#define _TILE_74 143
+#define _TILE_75 144
+#define _TILE_76 145
+#define _TILE_77 146
+#define _TILE_78 147
+#define _TILE_79 148
+#define _TILE_80 149
+#define _TILE_81 150
+#define _TILE_82 151
+#define _TILE_83 152
+#define _TILE_84 153
+#define _TILE_85 154
+#define _TILE_86 155
+#define _TILE_87 156
+#define _TILE_88 157
+#define _TILE_89 158
+#define _TILE_90 159
+
+
